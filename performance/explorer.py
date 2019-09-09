d873c00395ba773d03b28226464dfce841c70ccc
Arturo Filastò <arturo@filasto.net>
2019-09-09 22:17:16 +0200

    Refactor the request spawning

--- performance/explorer.py
+++ performance/explorer.py
@@ -10,24 +10,26 @@ class ExplorerTasks(TaskSet):
         self.client.get("/api/v1/measurements", params={'until': '2019-09-09', 'limit': 50})
 
     @task
-    def country_page(self):
-        """
-        http://api.ooni.io/api/_/website_networks?probe_cc=DZ
-        http://api.ooni.io/api/_/im_networks?probe_cc=DZ
-        http://api.ooni.io/api/_/vanilla_tor_stats?probe_cc=DZ
-        http://api.ooni.io/api/_/network_stats?probe_cc=DZ&limit=4&offset=0
-        http://api.ooni.io/api/_/website_urls?probe_cc=DZ&probe_asn=36947&limit=5&offset=0
+    def country_stats(self):
+        probe_cc = 'DZ'
+        self.client.get("/api/_/im_networks", params={'probe_cc': probe_cc})
+        self.client.get("/api/_/vanilla_tor_stats", params={'probe_cc': probe_cc})
+        self.client.get("/api/_/network_stats", params={'probe_cc': probe_cc, 'limit': 4, 'offset': 0})
 
-        http://api.ooni.io/api/_/website_stats?probe_cc=DZ&probe_asn=36947&input=http:%2F%2Fwww.emule.com%2F
-        http://api.ooni.io/api/_/website_stats?probe_cc=DZ&probe_asn=36947&input=http:%2F%2Fwww.tialsoft.com%2Fdownload%2F
-        ... x5
-        """
-        self.client.get("/api/_/website_networks", params={'probe_cc': 'DZ'})
-        self.client.get("/api/_/im_networks", params={'probe_cc': 'DZ'})
-        self.client.get("/api/_/im_networks", params={'probe_cc': 'DZ'})
-        self.client.get("/api/_/network_stats", params={'probe_cc': 'DZ', 'limit': 4, 'offset': 0})
-        self.client.get("/api/_/website_urls", params={'probe_cc': 'DZ', 'probe_asn': 36947})
-        self.client.get("/api/_/website_stats", params={'probe_cc': 'DZ', 'probe_asn': 36947, 'input': 'http:%2F%2Fwww.tialsoft.com%2Fdownload%2F'})
+    @task
+    def website_networks(self):
+        probe_cc = 'DZ'
+        j = self.client.get("/api/_/website_networks", params={'probe_cc': probe_cc}).json()
+        if len(j['results']) > 0:
+            probe_asn = j['results'][0]['probe_asn']
+            j2 = self.client.get("/api/_/website_urls",
+                                 params={'probe_cc': probe_cc,
+                                         'probe_asn': probe_asn}).json()
+            for r in j2['results']:
+                self.client.get("/api/_/website_stats",
+                                params={'probe_cc': probe_cc,
+                                        'probe_asn': probe_asn,
+                                        'input': r['input']})
 
     @task
     def search_measurements(self):
