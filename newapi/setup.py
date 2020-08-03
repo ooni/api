4ad0bf9a8841d99c712d7568124c718223839dea
Federico Ceratto <federico.ceratto@gmail.com>
2020-08-03 14:44:17 +0100

    Install files manually...

--- newapi/setup.py
+++ newapi/setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="ooniapi",
-    packages=["ooniapi"],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     entry_points={"console_scripts": ["ooniapi = measurements.cli:cli",]},
+    package_data={
+        "ooniapi": (
+            "*.adoc",
+            "templates/*",
+            "static/*/*",
+            "countries/country-list.json",
+        ),
+    },
 )
