df939ab3c3b8a7c4e64bddb1c70e3234252768d0
Federico Ceratto <federico@debian.org>
2021-07-14 20:51:04 +0100

    Update deps

--- newapi/build_runner.sh
+++ newapi/build_runner.sh
@@ -21,14 +21,18 @@ echo 'deb http://deb-ci.ooni.org unstable main' \
 apt-key adv --verbose --keyserver hkp://keyserver.ubuntu.com \
   --recv-keys "B5A08F01796E7F521861B449372D1FF271F2DD50"
 apt-get update
+# Keep this in sync with debian/control
+# hint: grep debdeps **/*.py
 apt-get install --no-install-recommends -qy \
   git \
   gunicorn3 \
   python3-boto3 \
+  python3-filelock \
   python3-flasgger \
   python3-flask \
   python3-flask-cors \
   python3-flask-restful \
+  python3-freezegun \
   python3-jwt \
   python3-lz4 \
   python3-psycopg2 \
