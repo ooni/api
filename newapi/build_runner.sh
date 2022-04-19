446d12f09f599603a524a604e6d6028224f4a78d
Arturo Filastò <arturo@filasto.net>
2022-04-19 15:18:54 +0200

    Add missing development deps to build_runner.sh

--- newapi/build_runner.sh
+++ newapi/build_runner.sh
@@ -37,7 +37,10 @@ apt-get install --no-install-recommends -qy \
   python3-jwt \
   python3-lz4 \
   python3-psycopg2 \
+  python3-freezegun \
   python3-pytest \
+  python3-pytest-mock \
+  python3-pytest-benchmark \
   python3-pytest-cov \
   python3-setuptools \
   python3-sqlalchemy \
