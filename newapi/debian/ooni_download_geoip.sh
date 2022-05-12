6f2aec14a116fa2fa68e4101432cf05766a5c841
Arturo Filastò <arturo@filasto.net>
2022-05-12 17:46:21 +0200

    Add logging to systemd and statsd
    Basic smoke test for the database files

--- newapi/debian/ooni_download_geoip.sh
+++ newapi/debian/ooni_download_geoip.sh
@@ -13,6 +13,7 @@ CC_URL="https://download.db-ip.com/free/dbip-country-lite-${TS}.mmdb.gz"
 ASN_STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" ${ASN_URL})
 if [ "$ASN_STATUS" != "200" ];then
     echo "$ASN_URL hasn't been updated yet"
+    echo "$ASN_URL hasn't been updated yet" | systemd-cat -t ooniapi-geoip
     # We exit with 0 to avoid systemd thinking the unit has failed
     exit 0
 fi
@@ -20,13 +21,31 @@ fi
 CC_STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" ${CC_URL})
 if [ "$CC_STATUS" != "200" ];then
     echo "$CC_URL hasn't been updated yet"
+    echo "$CC_URL hasn't been updated yet" | systemd-cat -t ooniapi-geoip
     # We exit with 0 to avoid systemd thinking the unit has failed
     exit 0
 fi
 
 mkdir -p /var/lib/ooniapi/
+
+echo "Updating GeoIP database from $CC_URL $ASN_URL" | systemd-cat -t ooniapi-geoip
 curl -o /var/lib/ooniapi/asn.mmdb.gz ${ASN_URL}
 gunzip /var/lib/ooniapi/asn.mmdb.gz
 curl -o /var/lib/ooniapi/cc.mmdb.gz ${CC_URL}
 gunzip /var/lib/ooniapi/cc.mmdb.gz
+
+# Basic smoke test of the geoip databases
+python <<EOF
+import geoip2.database
+with geoip2.database.Reader('/var/lib/ooniapi/cc.mmdb') as reader:
+    resp = reader.country('8.8.8.8')
+    assert resp is not None
+with geoip2.database.Reader('/var/lib/ooniapi/asn.mmdb') as reader:
+    resp = reader.asn('8.8.8.8')
+    assert resp is not None
+EOF
+
 echo $TS > /var/lib/ooniapi/geoipdbts
+echo -n "ooni_download_geoip:1|c" >/dev/udp/localhost/8125
+echo "Updated GeoIP databases" | systemd-cat -t ooniapi-geoip
+exit 0
